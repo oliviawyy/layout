f49c66ec09a84edffe4efe25f42fd42deeb4e5d0
oliviawyy <olivia.c@aluno.senai.br>
2026-04-16 10:56:04 -0300

    fazendo o js aqui

--- dark-ligth-mode2/script.js
+++ dark-ligth-mode2/script.js
@@ -1,9 +1,10 @@
 // Evento que é disparado quando o conteúdo do DOM (estrutura HTML) é totalmente carregado
-
-
-
+window.addEventListener('load', () => {
   // Verifica no localStorage se existe um tema salvo. Se não houver, define o tema padrão como 'light'
- 
+    let SalvarTema = localStorage.getItem('tema');
+    if (!SalvarTema){
+    SalvarTema = 'light';
+    }
 
   // Adiciona a classe correspondente ao tema (light ou dark) no corpo da página (body)
 
@@ -19,6 +20,8 @@
    
 
     // Adiciona a nova classe de tema ao body
-   
+    
 
     // Armazena o novo tema no localStorage para que a preferência do usuário seja mantida
+    localStorage.setItem('tema', SalvarTema);
+    });
