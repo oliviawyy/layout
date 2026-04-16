3ff604392f999bc6afbaf21e42685ccb01ae1cfd
oliviawyy <olivia.c@aluno.senai.br>
2026-04-16 10:44:31 -0300

    fazendo a logica do html e do css..

---
+++ dark-ligth-mode2/script.js
@@ -0,0 +1,24 @@
+// Evento que é disparado quando o conteúdo do DOM (estrutura HTML) é totalmente carregado
+
+
+
+  // Verifica no localStorage se existe um tema salvo. Se não houver, define o tema padrão como 'light'
+ 
+
+  // Adiciona a classe correspondente ao tema (light ou dark) no corpo da página (body)
+
+
+  // Seleciona o botão de alternar tema usando o ID 'toggle-theme' e adiciona um evento de clique
+  
+    
+    // Verifica qual tema está atualmente aplicado no body (light ou dark)
+    // Se o tema for 'light', muda para 'dark', caso contrário, muda para 'light'
+    
+
+    // Remove as classes de tema antigas ('light' e 'dark') para evitar conflito
+   
+
+    // Adiciona a nova classe de tema ao body
+   
+
+    // Armazena o novo tema no localStorage para que a preferência do usuário seja mantida
